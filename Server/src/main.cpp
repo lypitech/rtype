b8ef4c70f048b4bcaadefd8f200f1675ce07dae1
Lysandre B. <lysandre.boursette@epitech.eu>
2025-11-30 15:33:48 +0100

    feat(server): Drafted main to test libraries.

--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -1,5 +1,21 @@
-int main(int argc, char *argv[]) {
-    (void) argc;
-    (void) argv;
+#include "rtnt.h"
+#include "logger/Logger.h"
+#include "logger/Sinks/LogFileSink.h"
+
+int main(int argc, char* argv[])
+{
+    Logger::getInstance().addSink<logger::ConsoleSink>();
+    Logger::getInstance().addSink<logger::LogFileSink>("logs/latest.log");
+
+    Logger::initialize(
+        "R-Type Server",
+        argc, const_cast<const char**>(argv),
+        logger::BuildInfo::fromCMake()
+    );
+
+    rtnt::hello();
+
+    LOG_INFO("hello from server");
+
     return 0;
 }
