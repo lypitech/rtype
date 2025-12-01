c6f3f1c245806d105ba5e82991d1c5959f3924a0
Lysandre B. <lysandre.boursette@epitech.eu>
2025-12-01 11:26:09 +0100

    feat(lib>rteng): Drafted `rteng` library.

---
+++ lib/rteng/include/rteng.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace rteng
+{
+
+void hello();
+
+}
---
+++ lib/rteng/src/rteng.cpp
@@ -0,0 +1,13 @@
+#include "rteng.h"
+
+#include "logger/Logger.h"
+
+namespace rteng
+{
+
+void hello()
+{
+    LOG_DEBUG("Hi from rteng");
+}
+
+}
