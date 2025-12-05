3a48260f3b84dda10a9e5ba1da919e29db9559d1
nL1x <nathan.jeannot@epitech.eu>
2025-12-05 18:53:06 +0100

    test(lib/rtecs/sparse-set): Added destroy and clear sparse-set tests

--- lib/rtecs/tests/tests/SparseSet.cpp
+++ lib/rtecs/tests/tests/SparseSet.cpp
@@ -40,23 +40,23 @@ TEST(SparseSet_create, create_entity_with_id_zero) {
     rtecs::SparseSet<MyComponent> sparseSet;
     sparseSet.create(0, { .name = std::string("Hello world"), .age = 42 });
 
-    const rtecs::OptionalRef<MyComponent> component = sparseSet.getComponent(1);
+    const rtecs::OptionalRef<MyComponent> component = sparseSet.getComponent(0);
 
-    ASSERT_FALSE(sparseSet.has(0));
-    ASSERT_FALSE(component.has_value());
+    ASSERT_TRUE(sparseSet.has(0));
+    ASSERT_TRUE(component.has_value());
 }
 
-TEST(SparseSet_create, create_multiple_entities) {
+TEST(SparseSet_create, create_multiple_ordered_entities) {
     struct MyComponent {
         std::string name;
         int age;
     };
     rtecs::SparseSet<MyComponent> sparseSet;
 
-    for (int id = 1; id < 10; id++) {
+    for (int id = 0; id < 10; id++) {
         sparseSet.create(id);
     }
-    for (int id = 1; id < 10; id++) {
+    for (int id = 0; id < 10; id++) {
         const rtecs::OptionalRef<MyComponent> component = sparseSet.getComponent(id);
         ASSERT_TRUE(sparseSet.has(id));
         ASSERT_TRUE(component.has_value());
@@ -69,20 +69,77 @@ TEST(SparseSet_create, create_multiple_entities_with_random_id) {
         int age;
     };
 
-    srand(time(nullptr));
-    // Limits the number of page up to 64.
-    constexpr size_t maximumId = rtecs::SparseSet<MyComponent>::kPageSize * 64;
-    std::vector<size_t> selectedIds;
+    std::vector<size_t> entities{ 1, 20, 3400, 4297, 9821, 12023 };
     rtecs::SparseSet<MyComponent> sparseSet;
 
-    for (int i = 0; i < 10; i++) {
-        const rtecs::EntityID id = rand() % maximumId;
-        selectedIds.push_back(id);
+    for (const size_t id : entities) {
         sparseSet.create(id);
     }
-    for (const auto id : selectedIds) {
+    for (const auto id : entities) {
         const rtecs::OptionalRef<MyComponent> component = sparseSet.getComponent(id);
         ASSERT_TRUE(sparseSet.has(id));
         ASSERT_TRUE(component.has_value());
+    }
+}
+
+TEST(SparseSet_clear, clear_empty_sparseset) {
+    struct MyComponent {
+        std::string name;
+        int age;
+    };
+
+    rtecs::SparseSet<MyComponent> sparseSet;
+    sparseSet.clear();
+
+    ASSERT_FALSE(sparseSet.has(0));
+}
+
+TEST(SparseSet_clear, clear_filled_sparseset) {
+    struct MyComponent {
+        std::string name;
+        int age;
+    };
+
+    rtecs::SparseSet<MyComponent> sparseSet;
+
+    for (int id = 0; id < 10; id++) {
+        sparseSet.create(id);
+    }
+
+    for (int id = 0; id < 10; id++) {
+        EXPECT_TRUE(sparseSet.has(id));
+    }
+
+    sparseSet.clear();
+
+    for (int id = 0; id < 10; id++) {
+        ASSERT_FALSE(sparseSet.has(id));
+    }
+}
+
+TEST(SparseSet_destroy, destroy_entity) {
+    struct MyComponent {
+        std::string name;
+        int age;
+    };
+
+    rtecs::SparseSet<MyComponent> sparseSet;
+
+    for (int id = 0; id < 10; id++) {
+        sparseSet.create(id);
+    }
+
+    for (int id = 0; id < 10; id++) {
+        EXPECT_TRUE(sparseSet.has(id));
+    }
+
+    sparseSet.destroy(2);
+    ASSERT_FALSE(sparseSet.has(2));
+
+    for (int id = 0; id < 2; id++) {
+        ASSERT_TRUE(sparseSet.has(id));
+    }
+    for (int id = 3; id < 10; id++) {
+        ASSERT_TRUE(sparseSet.has(id));
     }
 }
