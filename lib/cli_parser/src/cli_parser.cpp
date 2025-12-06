aea7e43e63633caab7dcb876413c7e0d56ba1a1e
Pierre Marguerie <pierre.marguerie@epitech.eu>
2025-12-06 17:11:25 +0100

    feat(cli_parser): Added `cli_parser` implementation

---
+++ lib/cli_parser/src/cli_parser.cpp
@@ -0,0 +1,38 @@
+#include "../include/cli_parser.hpp"
+
+#include <iostream>
+
+namespace cli_parser {
+
+Parser::Parser(const int argc, const char* argv[]) {
+    for (int i = 1; i < argc; ++i) {
+        const std::string arg = argv[i];
+        if (arg[0] != '-') {
+            continue;
+        }
+        i++;
+        if (i >= argc) {
+            _flags.emplace(arg, "");
+            break;
+        }
+        if (argv[i][0] == '-') {
+            i--;
+            _flags.emplace(arg, "");
+        } else {
+            _flags.emplace(arg, std::string(argv[i]));
+        }
+    }
+}
+
+std::string Parser::getValue(const std::string& flag) {
+    if (_flags.contains(flag)) {
+        return _flags.at(flag);
+    }
+    return "";
+}
+
+bool Parser::hasFlag(const std::string& flag) const {
+    return _flags.contains(flag);
+}
+
+}  // namespace cli_parser
