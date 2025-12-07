b4b2e205739833458a89a7e8ec6afc0df1ce8982
Lysandre B. <lysandre.boursette@epitech.eu>
2025-12-07 18:56:15 +0100

    feat(lib>rtnt>core>packet>internal): Drafted internal packet `CONNECT`.

---
+++ lib/rtnt/include/rtnt/Core/Packets/Connect.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "rtnt/Core/InternalPackets.hpp"
+#include "rtnt/Core/Packet.hpp"
+
+namespace rtnt::core::packet::internal
+{
+
+struct Connect
+{
+    static constexpr Id   kId   = static_cast<uint16_t>(SystemMessageId::kConnect);
+    static constexpr Flag kFlag = Flag::kReliable;
+    static constexpr Name kName = "__rtnt_internal_CONNECT";
+
+    uint16_t protocolVer = PROTOCOL_VER;
+
+    template<typename Archive>
+    void serialize(Archive& ar)
+    {
+        ar & protocolVer;
+    }
+};
+
+}
