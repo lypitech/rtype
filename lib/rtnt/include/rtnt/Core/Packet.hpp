538b0ca0e78d697f1ff3abbc0ad2c69459c0538d
Lysandre B. <lysandre.boursette@epitech.eu>
2025-12-07 15:59:20 +0100

    refactor(lib>rtnt): Moved `Constants.hpp` in `Common/` folder.

--- lib/rtnt/include/rtnt/Core/Packet.hpp
+++ lib/rtnt/include/rtnt/Core/Packet.hpp
@@ -3,7 +3,7 @@
 #include <format>
 #include <vector>
 
-#include "rtnt/Constants.hpp"
+#include "rtnt/Common/Constants.hpp"
 
 namespace rtnt::core
 {
@@ -84,19 +84,6 @@ namespace packet
 }
 
 using ByteBuffer = std::vector<uint8_t>;
-
-inline std::string byteBufferToHexString(const ByteBuffer& buffer)
-{
-    std::string result = "[";
-    for (size_t i = 0; i < buffer.size(); ++i) {
-        if (i > 0) {
-            result += " ";
-        }
-        result += std::format("{:02X}", buffer[i]);
-    }
-    result += "]";
-    return result;
-}
 
 class Packet
 {
