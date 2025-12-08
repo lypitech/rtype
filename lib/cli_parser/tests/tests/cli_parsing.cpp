cc4ac11dd3fbb35e4bb0c102ad3655607839d453
Pierre Marguerie <pierre.marguerie@epitech.eu>
2025-12-08 17:30:21 +0100

    fix(lib/cli_parser): Removed pointless comments and renamed file

--- lib/cli_parser/tests/tests/cli_parsing.cpp
+++ lib/cli_parser/tests/tests/cli_parsing.cpp
@@ -10,21 +10,21 @@ TEST(cli_parsing, empty) {
 }
 
 TEST(cli_parsing, one_empty_flag) {
-    const char *argv[] = {"prog_name", "-p", nullptr}; // Utilisez NULL en C
+    const char *argv[] = {"prog_name", "-p", nullptr};
     cli_parser::Parser p(2, argv);
     EXPECT_TRUE(p.hasFlag("-p"));
     EXPECT_EQ(p.getValue("-p"), "");
 }
 
 TEST(cli_parsing, one_flag) {
-    const char *argv[] = {"prog_name", "-p", "p_flag", nullptr}; // Utilisez NULL en C
+    const char *argv[] = {"prog_name", "-p", "p_flag", nullptr};
     cli_parser::Parser p(3, argv);
     EXPECT_TRUE(p.hasFlag("-p"));
     EXPECT_EQ(p.getValue("-p"), "p_flag");
 }
 
 TEST(cli_parsing, multiple_flags_with_value) {
-    const char *argv[] = {"prog_name", "-p", "p_flag", "-h", "h_flag", nullptr}; // Utilisez NULL en C
+    const char *argv[] = {"prog_name", "-p", "p_flag", "-h", "h_flag", nullptr};
     cli_parser::Parser p(5, argv);
     EXPECT_TRUE(p.hasFlag("-p"));
     EXPECT_EQ(p.getValue("-p"), "p_flag");
@@ -33,14 +33,14 @@ TEST(cli_parsing, multiple_flags_with_value) {
 }
 
 TEST(cli_parsing, multiple_toggle_flags) {
-    const char *argv[] = {"prog_name", "-p", "-d", nullptr}; // Utilisez NULL en C
+    const char *argv[] = {"prog_name", "-p", "-d", nullptr};
     cli_parser::Parser p(3, argv);
     EXPECT_TRUE(p.hasFlag("-p"));
     EXPECT_TRUE(p.hasFlag("-d"));
 }
 
 TEST(cli_parsing, toggle_flag_and_value_flag) {
-    const char *argv[] = {"prog_name", "-p", "p_flag", "-d", nullptr}; // Utilisez NULL en C
+    const char *argv[] = {"prog_name", "-p", "p_flag", "-d", nullptr};
     cli_parser::Parser p(4, argv);
     EXPECT_TRUE(p.hasFlag("-p"));
     EXPECT_EQ(p.getValue("-p"), "p_flag");
