69d48e378277703ee4e9f77a5cd27f31b96865e0
nL1x <nathan.jeannot@epitech.eu>
2025-12-09 16:06:44 +0100

    feat(lib/rtecs): WIP

--- lib/rtecs/include/ASystem.hpp
+++ lib/rtecs/include/ASystem.hpp
@@ -34,18 +34,22 @@ class ASystem
 
 namespace rtecs {
 
-class ECS; // Forward declaration
+class ECS;  // Forward declaration
 
 class ASystem {
-private:
+   private:
     const BitMask _mask;
-protected:
+
+   protected:
     [[nodiscard]]
-    BitMask getMask() const noexcept { return _mask; };
-public:
-    explicit ASystem(const BitMask mask): _mask(mask) {};
+    BitMask getMask() const noexcept {
+        return _mask;
+    };
+
+   public:
+    explicit ASystem(const BitMask mask) : _mask(mask) {};
     virtual ~ASystem() = default;
     virtual void apply(ECS &ecs) = 0;
 };
 
-}
+}  // namespace rtecs
