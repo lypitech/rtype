1e9903eff833a1a170798e223fb877ba8d68eeef
ElectronicIV <louis.persin@epitech.eu>
2025-12-10 00:22:22 +0100

    refactor(lib/rtecs/ECS): link system to classID

--- lib/rtecs/src/ECS.cpp
+++ lib/rtecs/src/ECS.cpp
@@ -4,8 +4,6 @@
 
 using namespace rtecs;
 
-void ECS::registerSystem(const SystemID id, std::unique_ptr<ASystem> system) { _systemView[id] = std::move(system); }
-
 void ECS::applySystem(const SystemID id) { _systemView[id]->apply(*this); }
 
 void ECS::applyAllSystems()
