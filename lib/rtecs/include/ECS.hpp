c54f3699d1b6c4c81aef562b687a7b19d671ac83
nL1x <nathan.jeannot@epitech.eu>
2025-12-09 21:39:14 +0100

    feat(lib/rtecs): Added ECS
    Don't ask questions, we are hungry.
    Co-Authored-By: ElectronicIV <louis.persin@epitech.eu>

--- lib/rtecs/include/ECS.hpp
+++ lib/rtecs/include/ECS.hpp
@@ -132,51 +132,114 @@ class ECS final
 }  // namespace rtecs
 #include <bitset>
 
-#include "ASystem.hpp"
+#include <bitset>
+#include <map>
+
 #include "DynamicBitSet.hpp"
+#include "ISparseSet.hpp"
+#include "SparseSet.hpp"
+#include "SparseVectorView.hpp"
 #include "rtecs/types.hpp"
 
 namespace rtecs {
 
-class ECS;  // Forward declaration for system type
+class ASystem;  // Forward declaration for system type
 
-using Entity = BitMask;
+using Entity = DynamicBitSet;
 using SystemID = size_t;
 using ComponentID = decltype(typeid(ISparseSet).hash_code());
 
+template <typename Component>
+using ComponentGroup = std::vector<std::reference_wrapper<Component>>;
+
+template <typename... Components>
+using ComponentGroupList = std::tuple<ComponentGroup<Components>...>;
+
 class ECS final {
    private:
+    template <typename Component>
+    DynamicBitSet getComponentBitSet() const {
+        DynamicBitSet bitset;
+        const size_t hashcode = typeid(Component).hash_code();
+
+        bitset[_componentView.getDenseIndex(hashcode)] = true;
+        return bitset;
+    }
+
    public:
+    explicit ECS() = default;
+
+    template <typename... Components>
+    explicit ECS() {
+        (registerComponent<Components>(), ...);
+    }
+
+    ~ECS() = default;
+
+    template <typename... Components>
+    DynamicBitSet getComponentsBitSet() const {
+        return (getComponentBitSet<Components>(), ...);
+    }
+
+    template <typename Component, typename... TupleTypes>
+    static constexpr ComponentGroup<Component> &getComponentGroup(
+        std::tuple<TupleTypes...> &tuple) {
+        return std::get<ComponentGroup<Component>>(tuple);
+    }
+
     template <typename Component>
     void registerComponent() {
         const size_t hashcode = typeid(Component).hash_code();
-        _componentMap[hashcode] = SparseSet<Component>();
+        _componentView[hashcode] = SparseSet<Component>();
     }
 
-    EntityID registerEntity(Entity entity);
-    SystemID registerSystem(ASystem system);
+    template <typename... Components>
+    EntityID registerEntity() {
+        const DynamicBitSet entity(getComponentsBitSet<Components...>());
 
-    void applySystem(SystemID systemId);
-    void applyAllSystem();
+        _entityList.push_back(entity);
+        return _entityList.size() - 1;
+    }
+
+    void registerSystem(SystemID id, std::unique_ptr<ASystem> system);
+
+    void applySystem(SystemID id);
+    void applyAllSystems();
+
+    template <typename Component>
+    ISparseSet &getComponent() {
+        const size_t hashcode = typeid(Component).hash_code();
+        return _componentView[hashcode];
+    };
 
     template <typename... Components>
-    std::vector<Components> getAllComponents();
+    ComponentGroupList<Components...> getMultipleComponents() {
+        std::tuple<std::vector<Components>...> tuple{};
+        DynamicBitSet bitset;
+
+        ((bitset |= getComponentBitSet<Components>()), ...);
+        for (const auto &entity : _entityList) {
+            if ((entity & bitset) == bitset)
+                (std::get<std::vector<Components>>(tuple).push_back(
+                     getComponentOf<Components>(entity)),
+                 ...);
+        }
+        return tuple;
+    }
 
     template <typename Component>
     Component &getComponentOf(EntityID entity) {
         const ComponentID componentId = typeid(Component).hash_code();
-        auto sparseSet =
-            dynamic_cast<SparseSet<Component>>(_componentMap[componentId]);
+        auto sparseSet = dynamic_cast<SparseSet<Component>>(
+            _componentView.getDenseIndex(componentId));
 
-        return sparseSet.getComponent(entity);
+        return sparseSet.get(entity);
     }
-
-    std::vector<Entity> getAllEntities();
 
    private:
     std::vector<Entity> _entityList;
-    std::unordered_map<ComponentID, ISparseSet> _componentMap;
-    std::vector<ISparseSet> std::vector<ASystem> _systemList;
+    SparseVectorView<ComponentID, ISparseSet> _componentView;
+    SparseVectorView<SystemID, std::unique_ptr<ASystem>> _systemView;
 };
 
 }  // namespace rtecs
