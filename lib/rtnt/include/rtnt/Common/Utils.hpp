d96c659afc70e37bdb58379874092589356b714b
Lysandre B. <lysandre.boursette@epitech.eu>
2025-12-10 15:11:18 +0100

    feat(lib>rtnt>common>utils): Overloaded `byteBufferToHexString` function to accept position iterators (used to convert a single part of a given ByteBuffer).

--- lib/rtnt/include/rtnt/Common/Utils.hpp
+++ lib/rtnt/include/rtnt/Common/Utils.hpp
@@ -6,6 +6,23 @@ namespace rtnt
 {
 
     /**
+     * @brief   Converts a section of a ByteBuffer to a readable string in a hexadecimal form.
+     * Mainly used for logging.
+     *
+     * @note    Only goes from begin to end. If you want to convert an entire ByteBuffer, simply call the other
+                overload.
+     * @note    This function doesn't affect the buffer.
+     *
+     * @param   begin   Starting iterator (from)
+     * @param   end     Ending iterator (to)
+     * @return  Converted buffer
+     */
+    std::string byteBufferToHexString(
+        core::ByteBuffer::const_iterator begin,
+        core::ByteBuffer::const_iterator end
+    );
+
+    /**
      * @brief   Converts a ByteBuffer to a readable string in a hexadecimal form.
      * Mainly used for logging.
      *
