4bc1572a100200b3f191067ffc6020372413d90c
Pierre Marguerie <pierre.marguerie@epitech.eu>
2025-12-10 16:13:57 +0100

    ref(common>documentation): Changed documentation to fit with norm

--- common/enums/game_state.hpp
+++ common/enums/game_state.hpp
@@ -9,9 +9,9 @@ namespace game {
  */
 enum class State
 {
-    kGameOver = 1,   // The game is lost.
-    kGameStart = 2,  // Start the game.
-    kGameEnd = 3,    // The game is won.
+    kGameOver = 1,   ///< The game is lost.
+    kGameStart = 2,  ///< Start the game.
+    kGameEnd = 3,    ///< The game is won.
 };
 
 }  // namespace game
