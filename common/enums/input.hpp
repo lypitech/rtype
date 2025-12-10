0a7c45f9e1bbcb02d8587047ac1508269b175038
Pierre Marguerie <pierre.marguerie@epitech.eu>
2025-12-10 16:13:57 +0100

    ref(common>documentation): Changed documentation to fit with norm

--- common/enums/input.hpp
+++ common/enums/input.hpp
@@ -9,12 +9,12 @@ namespace game {
  */
 enum class Input
 {
-    kNone = 0,        // No input.
-    kUp = 1 << 0,     // Go up.
-    kDown = 1 << 1,   // Go down.
-    kLeft = 1 << 2,   // Go to the left.
-    kRight = 1 << 3,  // Go to the right.
-    kShoot = 1 << 4   // Shoot the primary weapon.
+    kNone = 0,        ///< No input.
+    kUp = 1 << 0,     ///< Go up.
+    kDown = 1 << 1,   ///< Go down.
+    kLeft = 1 << 2,   ///< Go to the left.
+    kRight = 1 << 3,  ///< Go to the right.
+    kShoot = 1 << 4   ///< Shoot the primary weapon.
 };
 
 }  // namespace game
