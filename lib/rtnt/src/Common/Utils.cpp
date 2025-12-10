145d45aba16b6dd90b3a0dd2f3eafb5a9ad1063f
Lysandre B. <lysandre.boursette@epitech.eu>
2025-12-10 15:11:18 +0100

    feat(lib>rtnt>common>utils): Overloaded `byteBufferToHexString` function to accept position iterators (used to convert a single part of a given ByteBuffer).

--- lib/rtnt/src/Common/Utils.cpp
+++ lib/rtnt/src/Common/Utils.cpp
@@ -3,17 +3,35 @@
 namespace rtnt
 {
 
+std::string byteBufferToHexString(
+    const core::ByteBuffer::const_iterator begin,
+    const core::ByteBuffer::const_iterator end
+)
+{
+    const size_t size = std::distance(begin, end);
+
+    if (size == 0) {
+        return "[]";
+    }
+
+    std::string result;
+    result.reserve(1 + size * 3 - 1 + 1);
+
+    result += '[';
+    for (auto it = begin; it != end; ++it) {
+        if (it != begin) {
+            result += ' ';
+        }
+        result += std::format("{:02X}", *it); // todo: optimization: manual hex conversion instead of calling performance-heavy std::format.
+    }
+    result += ']';
+
+    return result;
+}
+
 std::string byteBufferToHexString(const core::ByteBuffer &buffer)
 {
-    std::string result = "[";
-    for (size_t i = 0; i < buffer.size(); ++i) {
-        if (i > 0) {
-            result += " ";
-        }
-        result += std::format("{:02X}", buffer[i]);
-    }
-    result += "]";
-    return result;
+    return byteBufferToHexString(buffer.begin(), buffer.end());
 }
 
 }
