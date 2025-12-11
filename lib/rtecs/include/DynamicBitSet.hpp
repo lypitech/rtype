5dd6d7bc19195751b6c3b5f53d1d4cadb407e4f2
Pierre Marguerie <pierre.marguerie@epitech.eu>
2025-12-11 10:39:29 +0100

    fix(merge): Fixed merge conflict resolution error

--- lib/rtecs/include/DynamicBitSet.hpp
+++ lib/rtecs/include/DynamicBitSet.hpp
@@ -10,38 +10,19 @@ class DynamicBitSet
     std::vector<std::bitset<64>> _bitsets;
     size_t _nbits = 0;
 
-   public:
+public:
     class BitRef
     {
         std::bitset<64> &block;
         std::bitset<64> mask;
 
-       public:
-#include <vector>
-#include <bitset>
-#include <vector>
-
-namespace rtecs {
-
-class DynamicBitSet {
-    std::vector<std::bitset<64>> _bitsets;
-    size_t _nbits = 0;
-
-   public:
-    class BitRef
-    {
-        std::bitset<64> &block;
-        std::bitset<64> mask;
-
-       public:
+    public:
         explicit BitRef(std::bitset<64> &b, std::bitset<64> m);
 
         explicit operator bool() const;
 
         BitRef &operator=(bool v);
         bool operator==(const BitRef &) const;
-        BitRef& operator=(bool v);
-        BitRef& operator&(BitRef &other);
     };
 
     [[nodiscard]] bool any() const;
@@ -52,14 +33,6 @@ class DynamicBitSet {
 
     DynamicBitSet operator&(const DynamicBitSet &other) const;
     DynamicBitSet operator|(const DynamicBitSet &other) const;
-    BitRef operator[](size_t i);
-    bool operator[](size_t i) const;
-    bool operator==(const DynamicBitSet &) const;
-};
-
-}  // namespace rtecs
-    DynamicBitSet operator&(DynamicBitSet &other) const;
-    DynamicBitSet operator|(DynamicBitSet &other) const;
     BitRef operator[](size_t i);
     bool operator[](size_t i) const;
     bool operator==(const DynamicBitSet &) const;
--- lib/rtecs/include/ISparseSet.hpp
+++ lib/rtecs/include/ISparseSet.hpp
@@ -4,14 +4,10 @@ namespace rtecs {
 
 class ISparseSet
 {
-   public:
-    virtual ~ISparseSet() = default;
-
-    DynamicBitSet getMask();
 public:
     virtual ~ISparseSet() = default;
 
-    BitMask getMask();
+    DynamicBitSet getMask();
 
     /**
      * @brief Check if the sparse-set has the given entity.
@@ -22,7 +18,6 @@ public:
      */
     [[nodiscard]]
     virtual bool has(size_t id) const noexcept = 0;
-    virtual bool has(EntityID id) const noexcept = 0;
 
     /**
      * @brief Remove the entity associated component from the sparse-set.
@@ -30,7 +25,6 @@ public:
      * @param id The entity to remove from the sparse-set.
      */
     virtual void remove(size_t id) noexcept = 0;
-    virtual void remove(EntityID id) noexcept = 0;
 
     /**
      * Clear the sparse-set.
@@ -39,4 +33,3 @@ public:
 };
 
 }  // namespace rtecs
-}
