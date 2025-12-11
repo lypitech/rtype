2697841c86bb3bb5d7997bdef76649b280ce9932
Pierre Marguerie <pierre.marguerie@epitech.eu>
2025-12-11 15:02:05 +0100

    feat(client): Added basic `-h` implementation

--- Client/src/main.cpp
+++ Client/src/main.cpp
@@ -1,21 +1,23 @@
-#include "rtnt.h"
+#include "cli_parser.hpp"
 #include "logger/Logger.h"
 #include "logger/Sinks/LogFileSink.h"
 
-int main(int argc, char* argv[])
+int main(const int argc, const char* argv[])
 {
     Logger::getInstance().addSink<logger::ConsoleSink>();
     Logger::getInstance().addSink<logger::LogFileSink>("logs/client_latest.log");
 
-    Logger::initialize(
-        "R-Type Client",
-        argc, const_cast<const char**>(argv),
-        logger::BuildInfo::fromCMake()
-    );
+    Logger::initialize("R-Type Client", argc, argv, logger::BuildInfo::fromCMake());
 
-    rtnt::hello();
+    cli_parser::Parser p(argc, argv);
+
+    if (p.hasFlag("--help") || argc < 2) {
+        std::cout << "Usage: " << argv[0] << " [options]" << std::endl
+                  << "\t[options]: -h <host> (The ip address of the server)" << std::endl
+                  << "\t[options]: -p <port> (A short designating the port to listen to)" << std::endl;
+        return 0;
+    }
 
     LOG_INFO("hello from client");
-
     return 0;
 }
