46b0347a83c6fbfa90b4b13612681475dff2c782
Lysandre B. <lysandre.boursette@epitech.eu>
2025-12-11 12:20:10 +0100

    chore(lib>rtnt>core>dispatcher): Moved function in source file instead of header.

--- lib/rtnt/src/Core/Dispatcher.cpp
+++ lib/rtnt/src/Core/Dispatcher.cpp
@@ -10,4 +10,18 @@ Dispatcher::Dispatcher()
     _internal_bind<packet::internal::Connect>();
 }
 
+void Dispatcher::dispatch(
+    const std::shared_ptr<Session>& session,
+    Packet& packet
+)
+{
+    auto iterator = _handlers.find(packet.getId());
+
+    if (iterator != _handlers.end()) {
+        iterator->second(session, packet);
+    } else {
+        LOG_ERR("Trying to dispatch an unknown packet (#{}).", packet.getId()); // todo: better logging
+    }
+}
+
 }
