3e91437b316aa51078c1781849472bec5b096b8f
Pierre Marguerie <pierre.marguerie@epitech.eu>
2025-12-11 23:17:30 +0100

    ref(common>enums): Removed redondant value assigning

--- common/enums/packets.hpp
+++ common/enums/packets.hpp
@@ -10,9 +10,9 @@ namespace packet::type {
  */
 enum class Client : uint16_t
 {
-    kJoin = 0x128,       ///< Ask to join a game.
-    kStart = 0x129,      ///< Ask to start a game.
-    kUserInput = 0x130,  ///< Send the bitmask of the input.
+    kJoin = 0x128,  ///< Ask to join a game.
+    kStart,         ///< Ask to start a game.
+    kUserInput,     ///< Send the bitmask of the input.
 };
 
 /**
@@ -22,15 +22,15 @@ enum class Client : uint16_t
  */
 enum class Server : uint16_t
 {
-    kJoinAck = 0x150,            ///< Notify the status of a join request.
-    kPlayerJoin = 0x151,         ///< Notify that a player joined the lobby.
-    kSpawn = 0x152,              ///< Notify of an entity spawn.
-    kDestroy = 0x153,            ///< Notify of an entity destroying.
-    kUpdateEntityState = 0x154,  ///< Update the state of an entity.
-    kUpdatePosition = 0x155,     ///< Update the position of an entity.
-    kUpdateHealth = 0x156,       ///< Update the health of an entity.
-    kUpdateGameState = 0x157,    ///< Update the state of the game.
-    kWorldInit = 0x158,          ///< Init the world content.
+    kJoinAck = 0x150,    ///< Notify the status of a join request.
+    kPlayerJoin,         ///< Notify that a player joined the lobby.
+    kSpawn,              ///< Notify of an entity spawn.
+    kDestroy,            ///< Notify of an entity destroying.
+    kUpdateEntityState,  ///< Update the state of an entity.
+    kUpdatePosition,     ///< Update the position of an entity.
+    kUpdateHealth,       ///< Update the health of an entity.
+    kUpdateGameState,    ///< Update the state of the game.
+    kWorldInit,          ///< Init the world content.
 };
 
 }  // namespace packet::type
