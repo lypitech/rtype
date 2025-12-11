15da29a78955ca6b4f2f5ee5f8f51b6fa8b2f98c
Pierre Marguerie <pierre.marguerie@epitech.eu>
2025-12-11 09:41:52 +0100

    fix: Updated to new coding-style

--- lib/cli_parser/tests/Main.cpp
+++ lib/cli_parser/tests/Main.cpp
@@ -2,7 +2,8 @@
 
 #include "cli_parser.hpp"
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     testing::InitGoogleTest(&argc, argv);
 
     return RUN_ALL_TESTS();
--- lib/cli_parser/tests/tests/cli_parsing.cpp
+++ lib/cli_parser/tests/tests/cli_parsing.cpp
@@ -2,28 +2,32 @@
 
 #include "cli_parser.hpp"
 
-TEST(cli_parsing, empty) {
+TEST(cli_parsing, empty)
+{
     const char *argv[] = {"prog_name", nullptr};
     cli_parser::Parser p(1, argv);
     EXPECT_FALSE(p.hasFlag("-p"));
     EXPECT_EQ(p.getValue("-p").as<std::string>(), "");
 }
 
-TEST(cli_parsing, one_empty_flag) {
+TEST(cli_parsing, one_empty_flag)
+{
     const char *argv[] = {"prog_name", "-p", nullptr};
     cli_parser::Parser p(2, argv);
     EXPECT_TRUE(p.hasFlag("-p"));
     EXPECT_EQ(p.getValue("-p").as<std::string>(), "");
 }
 
-TEST(cli_parsing, one_flag) {
+TEST(cli_parsing, one_flag)
+{
     const char *argv[] = {"prog_name", "-p", "p_flag", nullptr};
     cli_parser::Parser p(3, argv);
     EXPECT_TRUE(p.hasFlag("-p"));
     EXPECT_EQ(p.getValue("-p").as<std::string>(), "p_flag");
 }
 
-TEST(cli_parsing, multiple_flags_with_value) {
+TEST(cli_parsing, multiple_flags_with_value)
+{
     const char *argv[] = {"prog_name", "-p", "p_flag", "-h", "h_flag", nullptr};
     cli_parser::Parser p(5, argv);
     EXPECT_TRUE(p.hasFlag("-p"));
@@ -32,14 +36,16 @@ TEST(cli_parsing, multiple_flags_with_value) {
     EXPECT_EQ(p.getValue("-h").as<std::string>(), "h_flag");
 }
 
-TEST(cli_parsing, multiple_toggle_flags) {
+TEST(cli_parsing, multiple_toggle_flags)
+{
     const char *argv[] = {"prog_name", "-p", "-d", nullptr};
     cli_parser::Parser p(3, argv);
     EXPECT_TRUE(p.hasFlag("-p"));
     EXPECT_TRUE(p.hasFlag("-d"));
 }
 
-TEST(cli_parsing, toggle_flag_and_value_flag) {
+TEST(cli_parsing, toggle_flag_and_value_flag)
+{
     const char *argv[] = {"prog_name", "-p", "p_flag", "-d", nullptr};
     cli_parser::Parser p(4, argv);
     EXPECT_TRUE(p.hasFlag("-p"));
