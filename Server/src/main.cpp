1d78a031d310de412cdf29c059c53aa0b268d194
Pierre Marguerie <pierre.marguerie@epitech.eu>
2025-12-12 23:37:17 +0100

    feat(server): Added `rteng` usage in server

--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -1,21 +1,18 @@
-#include "rtnt.h"
 #include "logger/Logger.h"
 #include "logger/Sinks/LogFileSink.h"
+#include "rteng.hpp"
 
 int main(int argc, char* argv[])
 {
     Logger::getInstance().addSink<logger::ConsoleSink>();
     Logger::getInstance().addSink<logger::LogFileSink>("logs/latest.log");
 
-    Logger::initialize(
-        "R-Type Server",
-        argc, const_cast<const char**>(argv),
-        logger::BuildInfo::fromCMake()
-    );
+    Logger::initialize("R-Type Server", argc, const_cast<const char**>(argv), logger::BuildInfo::fromCMake());
 
-    rtnt::hello();
-
-    LOG_INFO("hello from server");
+    rteng::GameEngine eng(4243);
+    eng.init();
+    eng.run();
+    LOG_INFO("Shutting down server.");
 
     return 0;
 }
