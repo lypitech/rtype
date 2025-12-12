95f43539b3348f83a26983f1807a26d32e2543e5
Pierre Marguerie <pierre.marguerie@epitech.eu>
2025-12-12 23:22:57 +0100

    ref(client): Removed useless callback functions

--- Client/src/callbacks/on_connect.cpp
+++
@@ -1,7 +0,0 @@
-#include "logger/Logger.h"
-
-namespace rt::callback {
-
-void onConnect() { LOG_DEBUG("Connected to peer."); }
-
-}  // namespace rt::callback
--- Client/src/callbacks/on_message.cpp
+++
@@ -1,8 +0,0 @@
-#include "logger/Logger.h"
-#include "rtnt/Core/packet.hpp"
-
-namespace rt::callback {
-
-void onMessage(rtnt::core::Packet&) { LOG_DEBUG("He talked to me !"); }
-
-}  // namespace rt::callback
