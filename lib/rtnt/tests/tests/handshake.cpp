70ce8beaf0d77d2ee3e631783d41d50182b940e2
Lysandre B. <lysandre.boursette@epitech.eu>
2025-12-13 13:18:28 +0100

    refactor(lib>rtnt>tests>handshake): Rewrote `Handshake` test.

--- lib/rtnt/tests/tests/handshake.cpp
+++ lib/rtnt/tests/tests/handshake.cpp
@@ -9,7 +9,7 @@
 #include "rtnt/core/client.hpp"
 #include "rtnt/core/server.hpp"
 
-TEST(handshake, basic)
+TEST(rtnt, handshake)
 {
     asio::io_context context;
     auto workGuard = asio::make_work_guard(context);
@@ -18,54 +18,31 @@ TEST(handshake, basic)
     rtnt::core::Server server(context, 4242);
     rtnt::core::Client client(context);
 
-    std::promise<bool> promise;
-    auto future = promise.get_future();
+    std::promise<bool> clientConnectedPromise;
+    std::future<bool> clientConnectedFuture = clientConnectedPromise.get_future();
 
-    LOG_DEBUG("Setting onMessage function of server");
-    // server.onMessage([](const auto& session, auto& packet) {
-    //     LOG_DEBUG("Server received a message!");
-    // });
-
-    server.onDisconnect([](const auto& session) {
-        LOG_DEBUG("Session {} disconnected", session.get()->getEndpoint().address().to_string());
+    server.onConnect([](const std::shared_ptr<rtnt::core::Session>& session) {
+        LOG_DEBUG("Server: New session ({}) connected from {}", session->getId(), session->getEndpoint().port());
     });
 
-    server.onConnect([](const auto& session) {
-        LOG_DEBUG("Session {} connected", session.get()->getEndpoint().address().to_string());
+    client.onConnect([&]() {
+        LOG_INFO("Client: Handshake complete, connected to server.");
+        clientConnectedPromise.set_value(true);
     });
 
-    LOG_DEBUG("Starting server");
     server.start();
 
-    LOG_DEBUG("Will run context in another thread");
     std::thread ioThread([&context]() {
         logger::setThreadLabel("IoThread");
-        LOG_DEBUG("Running context");
         context.run();
     });
 
-    LOG_DEBUG("Now waiting 400ms");
-    std::this_thread::sleep_for(std::chrono::milliseconds(400));
-
-    client.onConnect([&]() {
-        LOG_DEBUG("Handshake is done, client is connected!");
-        promise.set_value(true);
-    });
-
-    client.onDisconnect([]() { LOG_DEBUG("Client disconnected"); });
-
-    client.onMessage([](rtnt::core::Packet&) {});
-
-    LOG_DEBUG("Will connect");
     client.connect("127.0.0.1", 4242);
 
-    const std::future_status status = future.wait_for(std::chrono::seconds(2));
+    ASSERT_EQ(clientConnectedFuture.wait_for(std::chrono::seconds(3)), std::future_status::ready)
+        << "Test Failed: Client did not connect within timeout.";
 
-    EXPECT_EQ(status, std::future_status::ready) << "Test timed out!";
-
-    if (status == std::future_status::ready) {
-        EXPECT_TRUE(future.get());
-    }
+    EXPECT_TRUE(clientConnectedFuture.get()) << "Client failed to connect.";
 
     context.stop();
     if (ioThread.joinable()) {
