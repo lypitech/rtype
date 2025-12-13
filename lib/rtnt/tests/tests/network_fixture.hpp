1b2cceeec143d58ad3bbbb66054b469ff3133f59
Lysandre B. <lysandre.boursette@epitech.eu>
2025-12-13 18:21:16 +0100

    feat(lib>rtnt>tests): Added custom network fixture to automate client and server creation (and teardown).

---
+++ lib/rtnt/tests/tests/network_fixture.hpp
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <gtest/gtest.h>
+#include <asio/io_context.hpp>
+#include <thread>
+#include <future>
+
+#include "rtnt/Core/client.hpp"
+#include "rtnt/Core/server.hpp"
+#include "logger/Logger.h"
+
+class NetworkTest : public testing::Test {
+protected:
+    asio::io_context context;
+    asio::executor_work_guard<asio::io_context::executor_type> workGuard;
+
+    std::unique_ptr<rtnt::core::Server> server;
+    std::unique_ptr<rtnt::core::Client> client;
+
+    std::thread ioThread;
+
+    NetworkTest()
+        : workGuard(asio::make_work_guard(context))
+    {
+    }
+
+    void SetUp() override
+    {
+        server = std::make_unique<rtnt::core::Server>(context, 4242);
+        client = std::make_unique<rtnt::core::Client>(context);
+
+        server->start();
+
+        ioThread = std::thread([this]() {
+            context.run();
+        });
+    }
+
+    void TearDown() override
+    {
+        context.stop();
+
+        if (ioThread.joinable()) {
+            ioThread.join();
+        }
+
+        client.reset();
+        server.reset();
+
+        context.restart();
+    }
+
+    template <typename Func>
+    bool waitFor(Func condition, std::chrono::milliseconds timeout = std::chrono::seconds(2))
+    {
+        auto start = std::chrono::steady_clock::now();
+
+        while (std::chrono::steady_clock::now() - start < timeout) {
+            if (server) {
+                server->update();
+            }
+            if (client) {
+                client->update();
+            }
+
+            if (condition()) {
+                return true;
+            }
+            std::this_thread::sleep_for(std::chrono::milliseconds(5));
+        }
+        return false;
+    }
+};
