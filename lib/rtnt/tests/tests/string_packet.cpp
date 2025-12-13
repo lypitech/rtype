4a47578feebb22ddb512ca9b6d53b56c3a28531d
Lysandre B. <lysandre.boursette@epitech.eu>
2025-12-13 20:24:16 +0100

    tests(lib>rtnt): Created `StringPacket` test.

---
+++ lib/rtnt/tests/tests/string_packet.cpp
@@ -0,0 +1,43 @@
+#include <gtest/gtest.h>
+
+#include "network_fixture.hpp"
+
+namespace
+{
+
+struct Example
+{
+    static constexpr rtnt::core::packet::Id kId = 1801;
+    static constexpr rtnt::core::packet::Name kName = "EXAMPLE";
+
+    std::string str;
+
+    template <typename Archive>
+    void serialize(Archive& ar)
+    {
+        ar & str;
+    }
+};
+
+}
+
+TEST_F(NetworkTest, string_packet)
+{
+    constexpr std::string strToTransmit{ "hi lol" };
+    std::string receivedString{};
+
+    server->getPacketDispatcher().bind<Example>([&](const auto&, const Example& pkt) {
+        receivedString = pkt.str;
+    });
+
+    client->connect("127.0.0.1", 4242);
+
+    ASSERT_TRUE(waitFor([&]() { return client->isConnected(); })) << "Client failed to connect.";
+
+    constexpr Example ex{ .str = strToTransmit };
+    client->send(ex);
+
+    ASSERT_TRUE(waitFor([&]() { return !receivedString.empty(); })) << "Server has received no EXAMPLE packet.";
+
+    EXPECT_EQ(receivedString, strToTransmit);
+}
