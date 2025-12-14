244ea1ea7c10970d3be6342887d25abef221ae13
Pierre Marguerie <pierre.marguerie@epitech.eu>
2025-12-14 20:09:42 +0100

    feat(rteng>components): Added components serialization functions

--- lib/rteng/include/comp/Transform.hpp
+++ lib/rteng/include/comp/Transform.hpp
@@ -6,6 +6,12 @@ struct Transform
 {
     float x = 0;
     float y = 0;
+
+    template <typename Archive>
+    void serialize(Archive& ar)
+    {
+        ar & x & y;
+    }
 };
 
 }  // namespace comp
