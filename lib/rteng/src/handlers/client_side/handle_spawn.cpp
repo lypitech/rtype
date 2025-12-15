0eb3bbb78c9ec696441b150550fe24e18dab870d
Pierre Marguerie <pierre.marguerie@epitech.eu>
2025-12-15 01:40:06 +0100

    feat(rteng>network): Added `client_side` packet handlers

---
+++ lib/rteng/src/handlers/client_side/handle_spawn.cpp
@@ -0,0 +1,21 @@
+#include "handlers.hpp"
+#include "rteng.hpp"
+
+namespace rteng::client_side::handlers {
+
+void handleSpawn(const SessionPtr&, const packet::Spawn& packet)
+{
+    const rtecs::DynamicBitSet bitset(packet.bitmask);
+    BindingMap& binding_map = GameEngine::getInstance().getBindingMap();
+    const std::unique_ptr<rtecs::ECS>& ecs = GameEngine::getInstance().getEcs();
+
+    if (binding_map.contains(packet.id)) {
+        LOG_TRACE_R3("Entity has already been created, ignoring...");
+        return;
+    }
+    const rtecs::EntityID real = ecs->registerEntity(bitset);
+    binding_map.emplace(packet.id, real);
+    GameEngine::getInstance().getFactory().apply(*ecs, real, bitset, packet.content);
+}
+
+}  // namespace rteng::client_side::handlers
