07f03bad89cccab5c241a84e9ec893dced93fbfa
Pierre Marguerie <pierre.marguerie@epitech.eu>
2025-12-15 12:34:01 +0100

    feat: Added cli_parser usage in `Client` and `Server`

--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -1,15 +1,17 @@
+#include "cli_parser.hpp"
 #include "logger/Logger.h"
 #include "logger/Sinks/LogFileSink.h"
 #include "rteng.hpp"
 
-int main(int argc, char* argv[])
+int main(int argc, const char** argv)
 {
     Logger::getInstance().addSink<logger::ConsoleSink>();
     Logger::getInstance().addSink<logger::LogFileSink>("logs/latest.log");
 
     Logger::initialize("R-Type Server", argc, const_cast<const char**>(argv), logger::BuildInfo::fromCMake());
+    cli_parser::Parser p(argc, argv);
 
-    rteng::GameEngine eng(4243);
+    rteng::GameEngine eng(p.getValue("-p").as<int>());
     eng.init();
     eng.run();
     LOG_INFO("Shutting down server.");
