70c31debe81b364633fbfeffae8797c8a517e1f8
Pierre Marguerie <pierre.marguerie@epitech.eu>
2025-12-15 10:35:44 +0100

    fix(packet>handlers): Removed debug log

--- lib/rteng/src/handlers/client_side/handle_update_position.cpp
+++ lib/rteng/src/handlers/client_side/handle_update_position.cpp
@@ -9,18 +9,10 @@ void handleUpdatePosition(const SessionPtr&, const packet::UpdatePosition& packe
     rtecs::EntityID id = GameEngine::getInstance().getBindingMap().at(packet.id);
     rtecs::ISparseSet& positions = GameEngine::getInstance().getEcs()->getComponent<comp::Position>();
 
-    float old_x, old_y;
     if (const auto& position = dynamic_cast<rtecs::SparseSet<comp::Position>&>(positions).get(id)) {
-        LOG_DEBUG("Found position, updating...");
         auto& [x, y] = position.value().get();
-        old_x = x;
-        old_y = y;
         x = packet.position_x;
         y = packet.position_y;
-    }
-    if (const auto& position = dynamic_cast<rtecs::SparseSet<comp::Position>&>(positions).get(id)) {
-        auto& [x, y] = position.value().get();
-        LOG_DEBUG("Moved from ({}, {}) to ({}, {})", old_x, old_y, x, y);
     }
 }
 
