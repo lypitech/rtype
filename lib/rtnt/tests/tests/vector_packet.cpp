509cce585c64a6d88e5fcf6dffeace3b2fc08b9d
Lysandre B. <lysandre.boursette@epitech.eu>
2025-12-15 09:50:32 +0100

    feat(lib>rtnt>test>vector_packet): Added second data to test false packet underflow.

--- lib/rtnt/tests/tests/vector_packet.cpp
+++ lib/rtnt/tests/tests/vector_packet.cpp
@@ -10,11 +10,12 @@ struct Example
     static constexpr rtnt::core::packet::Name kName = "EXAMPLE";
 
     std::vector<uint8_t> data;
+    std::vector<uint8_t> data2;
 
     template <typename Archive>
     void serialize(Archive& ar)
     {
-        ar & data;
+        ar & data & data2;
     }
 };
 
@@ -22,19 +23,22 @@ struct Example
 
 TEST_F(NetworkTest, vector_packet)
 {
-    std::vector<uint8_t> dataToTransmit{ 13, 6, 8, 23, 4, 68 };
-    std::vector<uint8_t> receivedData{};
+    std::vector<uint8_t> dataToTransmit1{ 13, 6, 8 };
+    std::vector<uint8_t> dataToTransmit2{ 23, 4, 68, 28, 1, 60, 4, 9, 3 };
+    std::vector<uint8_t> receivedData1{};
+    std::vector<uint8_t> receivedData2{};
 
-    server->getPacketDispatcher().bind<Example>([&](const auto&, const Example& pkt) { receivedData = pkt.data; });
+    server->getPacketDispatcher().bind<Example>([&](const auto&, const Example& pkt) { receivedData1 = pkt.data; receivedData2 = pkt.data2; });
 
     client->connect("127.0.0.1", 4242);
 
     ASSERT_TRUE(waitFor([&]() { return client->isConnected(); })) << "Client failed to connect.";
 
-    Example ex{.data = dataToTransmit};
+    Example ex{.data = dataToTransmit1, .data2 = dataToTransmit2};
     client->send(ex);
 
-    ASSERT_TRUE(waitFor([&]() { return !receivedData.empty(); })) << "Server has received no EXAMPLE packet.";
+    ASSERT_TRUE(waitFor([&]() { return !receivedData1.empty() && !receivedData2.empty(); })) << "Server has received no EXAMPLE packet.";
 
-    EXPECT_EQ(receivedData, dataToTransmit);
+    EXPECT_EQ(receivedData1, dataToTransmit1);
+    EXPECT_EQ(receivedData2, dataToTransmit2);
 }
