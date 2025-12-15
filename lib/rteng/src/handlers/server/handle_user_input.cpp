f5c86518dcbe0c769ab0ab6efc9c5be5236358b2
Pierre Marguerie <pierre.marguerie@epitech.eu>
2025-12-15 01:40:26 +0100

    feat(rteng>network): Added `server_side` packet handlers

---
+++ lib/rteng/src/handlers/server/handle_user_input.cpp
@@ -0,0 +1,27 @@
+#include "enums/input.hpp"
+#include "handlers.hpp"
+#include "packets/client/user_input.hpp"
+#include "packets/server/update_position.hpp"
+#include "rteng.hpp"
+
+namespace rteng::server_side::handlers {
+
+void handleUserInput(const SessionPtr& s, const packet::UserInput& packet)
+{
+    const rtecs::EntityID id = GameEngine::getInstance().getSrvBindingMap().at(s->getId());
+    rtecs::ISparseSet& positions = GameEngine::getInstance().getEcs()->getComponent<comp::Position>();
+
+    if (const auto& position = dynamic_cast<rtecs::SparseSet<comp::Position>&>(positions).get(id)) {
+        auto& [x, y] = position.value().get();
+
+        x += (packet.input_mask & static_cast<uint8_t>(game::Input::kRight)) ? 10.0f : 0.0f;
+        x -= (packet.input_mask & static_cast<uint8_t>(game::Input::kLeft)) ? 10.0f : 0.0f;
+        y += (packet.input_mask & static_cast<uint8_t>(game::Input::kDown)) ? 10.0f : 0.0f;
+        y -= (packet.input_mask & static_cast<uint8_t>(game::Input::kUp)) ? 10.0f : 0.0f;
+        GameEngine::getInstance().getServer()->sendTo(
+            s, packet::UpdatePosition{static_cast<uint32_t>(id), static_cast<uint16_t>(x), static_cast<uint16_t>(y), 0,
+                                      0});
+    }
+}
+
+}  // namespace rteng::server_side::handlers
