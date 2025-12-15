28057abe9a6e695d0cbdebb83f86662cf24adee7
Pierre Marguerie <pierre.marguerie@epitech.eu>
2025-12-15 01:40:26 +0100

    feat(rteng>network): Added `server_side` packet handlers

---
+++ lib/rteng/src/handlers/server/handlers.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <memory>
+
+#include "packets/client/user_input.hpp"
+#include "rtnt/core/session.hpp"
+
+namespace rteng::server_side::handlers {
+
+using SessionPtr = std::shared_ptr<rtnt::core::Session>;
+
+void handleUserInput(const SessionPtr&, const packet::UserInput& packet);
+
+}  // namespace rteng::server_side::handlers
