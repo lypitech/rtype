14569820e693b535825535952e8ecee5fe2d7233
Pierre Marguerie <pierre.marguerie@epitech.eu>
2025-12-15 15:44:28 +0100

    fix(rteng): Overloaded raylib `Color` class

--- lib/rteng/include/comp/rect.hpp
+++ lib/rteng/include/comp/rect.hpp
@@ -1,22 +1,27 @@
 #pragma once
 
-#include <raylib.h>
-
 namespace comp {
+
+struct MyColor
+{
+    unsigned char r;  // Color red value
+    unsigned char g;  // Color green value
+    unsigned char b;  // Color blue value
+    unsigned char a;  // Color alpha value
+};
 
 struct Rectangle
 {
     bool shown = true;
     float width = 0.0f;
     float height = 0.0;
-    Color outline = {0, 0, 0, 0};
-    Color inFill = {0, 0, 0, 0};
+    MyColor outline = {0, 0, 0, 0};
+    MyColor inFill = {0, 0, 0, 0};
 
     template <typename Archive>
     void serialize(Archive& ar)
     {
-        ar & shown & width & height & outline.r & outline.g & outline.b & outline.a & inFill.r & inFill.g & inFill.b &
-            inFill.a;
+        ar & shown & width & height & outline & inFill;
     }
 };
 
