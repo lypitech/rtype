d208e97cfc4dc3273a0049115e13d04d2e91c430
Pierre Marguerie <pierre.marguerie@epitech.eu>
2025-12-15 12:28:43 +0100

    feat(rteng>packet>handlers): Added `WorldInit` packet handler

--- lib/rteng/src/handlers/client_side/handlers.hpp
+++ lib/rteng/src/handlers/client_side/handlers.hpp
@@ -3,6 +3,7 @@
 
 #include "packets/server/spawn.hpp"
 #include "packets/server/update_position.hpp"
+#include "packets/server/world_init.hpp"
 #include "rtnt/core/session.hpp"
 
 namespace rteng::client_side::handlers {
@@ -11,5 +12,6 @@ using SessionPtr = std::shared_ptr<rtnt::core::Session>;
 
 void handleSpawn(const SessionPtr&, const packet::Spawn& packet);
 void handleUpdatePosition(const SessionPtr&, const packet::UpdatePosition& packet);
+void handleWorldInit(const SessionPtr&, const packet::WorldInit& packet);
 
 }  // namespace rteng::client_side::handlers
