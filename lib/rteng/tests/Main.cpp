2d385f26f106db4e45572b71b3b35b5eb7f81ae1
Babouye <esteban.bouyault-yvanez@epitech.eu>
2025-12-16 16:09:00 +0100

    refactor(lib>rteng>tests): Commented every deprecated tests

--- lib/rteng/tests/Main.cpp
+++ lib/rteng/tests/Main.cpp
@@ -1,26 +1,3 @@
 #include <gtest/gtest.h>
 
-#include "../include/rteng.hpp"
-#include "TestEnvironment.hpp"
-
-// Add a flag or argument to control execution type
-int main(int argc, char* argv[])
-{
-    bool is_automated_test = false;  // Implement a check here if possible
-
-    if (argc == 2 && strcmp(argv[1], "--test") == 0) {
-        is_automated_test = true;
-    }
-
-    if (is_automated_test) {
-        testing::AddGlobalTestEnvironment(new RaylibTestEnvironment);
-        testing::InitGoogleTest(&argc, argv);
-        return RUN_ALL_TESTS();
-    }
-
-    // --- Interactive Mode ---
-    rteng::GameEngine engine(800, 600, "R-Type Client Interactive");
-    engine.run();
-
-    return 0;
-}
+int main() { return 0; }
--- lib/rteng/tests/tests/Initialization.cpp
+++ lib/rteng/tests/tests/Initialization.cpp
@@ -1,10 +1,11 @@
-#include <gtest/gtest.h>
-
-#include "../../include/rteng.hpp"
-
-TEST(Initialisation, EngineConstructor)
-{
-    rteng::GameEngine game(800, 600, "rteng Test Runner");
-
-    EXPECT_TRUE(true);
-}
+// #include <gtest/gtest.h>
+//
+// #include "../../include/rteng.hpp"
+//
+// TEST(Initialisation, EngineConstructor)
+// {
+// rteng::GameEngine game(800, 600, "rteng Test Runner");
+//
+// EXPECT_TRUE(true);
+// }
+//
