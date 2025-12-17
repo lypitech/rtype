90120acf2b6c0b3798a463286c289d0171c0beea
Babouye <esteban.bouyault-yvanez@epitech.eu>
2025-12-17 10:37:35 +0100

    refactor(server>format): Fixed style

--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -3,12 +3,14 @@
 #include "logger/Sinks/LogFileSink.h"
 #include "rteng.hpp"
 
-int main(int argc, const char** argv)
+int main(int argc,
+         const char** argv)
 {
     Logger::getInstance().addSink<logger::ConsoleSink>();
     Logger::getInstance().addSink<logger::LogFileSink>("logs/latest.log");
 
-    Logger::initialize("R-Type Server", argc, const_cast<const char**>(argv), logger::BuildInfo::fromCMake());
+    Logger::initialize(
+        "R-Type Server", argc, const_cast<const char**>(argv), logger::BuildInfo::fromCMake());
     cli_parser::Parser p(argc, argv);
 
     rteng::GameEngine eng(p.getValue("-p").as<int>());
