d2bf4f9209cc4639888f64653b369fa627f42f99
Pierre Marguerie <pierre.marguerie@epitech.eu>
2025-12-23 00:02:18 +0100

    fix(lib>rteng): Moved templated function from `.cpp` tp `.hpp`

--- lib/rteng/src/rteng.cpp
+++ lib/rteng/src/rteng.cpp
@@ -5,29 +5,6 @@
 
 namespace rteng {
 
-template <typename... Components>
-rtecs::EntityID GameEngine::registerEntity(
-    const std::shared_ptr<behaviour::MonoBehaviour>& mono_behaviour,
-    Components&&... components)
-{
-    const rtecs::EntityID entityId =
-        _ecs->registerEntity<std::decay_t<Components>...>(std::forward<Components>(components)...);
-
-    if (!mono_behaviour || !_ecs->hasEntityComponent<comp::Behaviour>(entityId)) {
-        return entityId;
-    }
-    auto& behaviourComponents = _ecs->getComponent<comp::Behaviour>();
-    auto& behaviourSparseSet =
-        dynamic_cast<rtecs::SparseSet<comp::Behaviour>&>(behaviourComponents);
-
-    comp::Behaviour behaviourComp;
-    behaviourComp.instance = mono_behaviour;
-    behaviourComp.started = false;
-
-    behaviourSparseSet.put(entityId, behaviourComp);
-    return entityId;
-}
-
 void GameEngine::runOnce(const double dt) const
 {
     {
