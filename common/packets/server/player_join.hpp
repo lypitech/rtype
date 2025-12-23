eeb5bb5e679ed1db8813f9b7f7f3dd0c4087831b
Pierre Marguerie <pierre.marguerie@epitech.eu>
2025-12-23 16:57:38 +0100

    ref(common>packets>server): Removed unnecessary `using` statement

--- common/packets/server/player_join.hpp
+++ common/packets/server/player_join.hpp
@@ -4,8 +4,6 @@
 
 namespace packet {
 
-using packetId = rtnt::core::packet::Id;
-
 /**
  * @struct packet::PlayerJoin
  *
@@ -14,7 +12,7 @@ using packetId = rtnt::core::packet::Id;
  */
 struct PlayerJoin
 {
-    static constexpr auto kId = static_cast<packetId>(type::Server::kPlayerJoin);
+    static constexpr auto kId = static_cast<rtnt::core::packet::Id>(type::Server::kPlayerJoin);
     static constexpr auto kFlag = rtnt::core::packet::Flag::kReliable;
     static constexpr rtnt::core::packet::Name kName = "PLAYER_JOIN";
 
