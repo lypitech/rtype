b72acdaf96b92c409bcf1b33be8c23ae90603ae1
Pierre Marguerie <pierre.marguerie@epitech.eu>
2025-12-23 11:59:50 +0100

    feat(server>documentation): Added stringdoc

--- Server/src/lobby/lobby.hpp
+++ Server/src/lobby/lobby.hpp
@@ -12,24 +12,66 @@ using Callback = std::function<void(rteng::GameEngine&)>;
 
 }  // namespace lobby
 
+/**
+ * @class Lobby
+ * @brief Encapsulates a gameEngine instance and it's networking interface
+ */
 class Lobby
 {
 public:
+    /**
+     * @brief Creates a lobby with the specified @code id@endcode.
+     * @param id an uint32(lobby::Id) that represents the id of the lobby.
+     *
+     * Note that the uniqueness of the ID depends on the user providing a distinct value.
+     */
     explicit Lobby(lobby::Id id);
 
+    /**
+     * @brief Tries to join this lobby.
+     * @param sessionId The id of the session trying to join.
+     * @return A boolean representing the status of the request.
+     */
     bool join(rtnt::core::session::Id sessionId);
+
+    /**
+     * @return The id of this lobby.
+     */
     lobby::Id getRoomId() const;
+
+    /**
+     * @brief Removes the @code SessionId@endcode from this lobby.
+     * @param sessionId The id of the session to remove.
+     */
     void leave(rtnt::core::session::Id sessionId);
+    /**
+     * @param sessionId The researched id.
+     * @return Whether this lobby contains this @code sessionId@endcode.
+     */
     bool hasJoined(rtnt::core::session::Id sessionId) const;
+
+    /**
+     * @brief Pushes a task to be made inside the running thread.
+     * This function is thread-safe.
+     * @param action A function performing the required action.
+     */
     void pushTask(lobby::Callback action);
+
+    /**
+     * @brief Start this lobby.
+     */
     void start();
+
+    /**
+     * @brief Stop this lobby.
+     */
     void stop();
 
 private:
     lobby::Id _roomId;
     utils::ConcurrentQueue<lobby::Callback> _actionQueue;
     rteng::GameEngine _engine;
-    std::unordered_map<rtnt::core::session::Id, size_t> _players;
+    std::unordered_map<rtnt::core::session::Id, rtecs::EntityID> _players;
     std::atomic<bool> _isRunning;
     std::thread _thread;
 
--- Server/src/lobby/lobby_manager.hpp
+++ Server/src/lobby/lobby_manager.hpp
@@ -5,18 +5,45 @@
 
 namespace lobby {
 
+/**
+ * @class Manager
+ * @brief A simple lobby manager
+ */
 class Manager
 {
 public:
     explicit Manager() = default;
     ~Manager();
 
+    /**
+     * @brief Tries to join a specific lobby.
+     * @param sessionId The id of the session trying to join.
+     * @param roomId The id of the lobby to join.
+     * @return Whether the request is successful.
+     */
     [[nodiscard]] bool joinRoom(rtnt::core::session::Id sessionId,
-                                lobby::Id roomId = 0) const;
+                                Id roomId = 0) const;
+    /**
+     * @brief Stops all lobbies.
+     */
     void stopAll() const;
+    /**
+     * @brief Leaves the lobby corresponding to this sessionId.
+     * @param sessionId The id of the disconnecting session.
+     */
     void leaveRoom(rtnt::core::session::Id sessionId);
+    /**
+     * @brief Pushes an action to be performed by the lobby.
+     * @param sessionId The id of the session triggering the action.
+     * @param action A function performing the triggered action.
+     */
     void pushActionToLobby(rtnt::core::session::Id sessionId,
                            Callback action);
+    /**
+     * @brief Creates a new lobby.
+     * The creation of the lobbies is not automatic.
+     * @return The id of the newly created lobby.
+     */
     Id createLobby();
 
 private:
