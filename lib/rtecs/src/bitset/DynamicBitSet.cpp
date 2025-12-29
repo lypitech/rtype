68ef00526b9d63d5cef939c2dc52356e1b125470
nL1x <nathan.jeannot@epitech.eu>
2025-12-29 22:42:17 +0100

    style(lib/rtecs/bitset): Fixed coding-style in the DynamicBitSet files

--- lib/rtecs/src/bitset/DynamicBitSet.cpp
+++ lib/rtecs/src/bitset/DynamicBitSet.cpp
@@ -2,8 +2,8 @@
 
 #include <algorithm>
 #include <ranges>
-#include <vector>
 #include <sstream>
+#include <vector>
 
 #include "logger/Logger.h"
 
@@ -39,9 +39,10 @@ DynamicBitSet::DynamicBitSet(const std::vector<uint8_t>& bytes)
     }
 }
 
-DynamicBitSet::DynamicBitSet(const std::vector<std::bitset<64>>& bitsets):
-    _bitsets(bitsets)
-{}
+DynamicBitSet::DynamicBitSet(const std::vector<std::bitset<64>>& bitsets)
+    : _bitsets(bitsets)
+{
+}
 
 void DynamicBitSet::applyOperation(const Operation& operation, DynamicBitSet& result, const DynamicBitSet& other) const
 {
@@ -62,8 +63,9 @@ void DynamicBitSet::leftShift(size_t nb)
 {
     for (; nb > 0; nb--) {
         for (auto it = _bitsets.begin(); it != _bitsets.end(); ++it) {
-            if (it != _bitsets.begin() && (*it)[63] == true)
+            if (it != _bitsets.begin() && (*it)[63] == true) {
                 (*(it - 1))[0] = true;
+            }
             *it <<= 1;
         }
     }
@@ -73,8 +75,9 @@ void DynamicBitSet::rightShift(size_t nb)
 {
     for (; nb > 0; nb--) {
         for (auto it = _bitsets.rbegin(); it != _bitsets.rend(); ++it) {
-            if (it != _bitsets.rbegin() && (*it)[0] == true)
+            if (it != _bitsets.rbegin() && (*it)[0] == true) {
                 (*(it - 1))[63] = true;
+            }
             *it >>= 1;
         }
     }
@@ -100,42 +103,31 @@ std::string DynamicBitSet::toString(const std::string& sep) const
 
     for (auto it = _bitsets.begin(); it != _bitsets.end(); ++it) {
         stream << it->to_string();
-        if (it + 1 != _bitsets.end())
+        if (it + 1 != _bitsets.end()) {
             stream << sep;
+        }
     }
     return stream.str();
 }
 
-size_t DynamicBitSet::capacity() const
-{
-    return _bitsets.size() * 64;
-}
+size_t DynamicBitSet::capacity() const { return _bitsets.size() * 64; }
 
 bool DynamicBitSet::any() const
 {
-    return std::ranges::any_of(
-        _bitsets.begin(),
-        _bitsets.end(),
-        [](const std::bitset<64> bitset) { return bitset.any(); }
-    );
+    return std::ranges::any_of(_bitsets.begin(), _bitsets.end(),
+                               [](const std::bitset<64> bitset) { return bitset.any(); });
 };
 
 bool DynamicBitSet::all() const
 {
-    return std::ranges::all_of(
-        _bitsets.begin(),
-        _bitsets.end(),
-        [](const std::bitset<64> bitset) { return bitset.all(); }
-    );
+    return std::ranges::all_of(_bitsets.begin(), _bitsets.end(),
+                               [](const std::bitset<64> bitset) { return bitset.all(); });
 }
 
 bool DynamicBitSet::none() const
 {
-    return std::ranges::all_of(
-        _bitsets.begin(),
-        _bitsets.end(),
-        [](const std::bitset<64> bitset) { return bitset.none(); }
-    );
+    return std::ranges::all_of(_bitsets.begin(), _bitsets.end(),
+                               [](const std::bitset<64> bitset) { return bitset.none(); });
 };
 
 void DynamicBitSet::clear()
@@ -221,7 +213,7 @@ bool DynamicBitSet::operator[](const size_t i) const
     if (i >= capacity()) {
         return false;
     }
-    return _bitsets[ DYN_BLOCK_INDEX(i) ][ DYN_BIT_INDEX(i) ];
+    return _bitsets[DYN_BLOCK_INDEX(i)][DYN_BIT_INDEX(i)];
 }
 
 bool DynamicBitSet::operator==(const DynamicBitSet& other) const
@@ -233,8 +225,9 @@ bool DynamicBitSet::operator==(const DynamicBitSet& other) const
     for (size_t i = 0; i < limit; i++) {
         const bool bitA = (i < currentCapacity) ? _bitsets[i / 64][i % 64] : false;
         const bool bitB = (i < otherCapacity) ? other._bitsets[i / 64][i % 64] : false;
-        if (bitA != bitB)
+        if (bitA != bitB) {
             return false;
+        }
     }
     return true;
 }
