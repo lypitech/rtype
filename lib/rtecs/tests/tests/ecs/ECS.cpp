b5f4ba5d51b29885d21a21c667335cc6213ba6a4
nL1x <nathan.jeannot@epitech.eu>
2026-01-07 22:38:05 +0100

    test(lib/rtecs/ecs): Added an empty ECS test file

---
+++ lib/rtecs/tests/tests/ecs/ECS.cpp
@@ -0,0 +1,13 @@
+#include "rtecs/ECS.hpp"
+
+#include <gtest/gtest.h>
+
+#include "logger/Logger.h"
+#include "rtecs/sparse/group/SparseGroup.hpp"
+
+using namespace rtecs;
+
+// TEST(ECS, test)
+// {
+//
+// };
