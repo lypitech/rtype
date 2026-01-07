012b66af34b68723d6603112542cea03a19bd61d
nL1x <nathan.jeannot@epitech.eu>
2026-01-07 22:31:58 +0100

    feat(lib/rtecs/ecs): Added multiple basic register and getter methods on the ECS class

--- lib/rtecs/src/ECS.cpp
+++ lib/rtecs/src/ECS.cpp
@@ -4,3 +4,14 @@
 
 using namespace rtecs;
 
+void ECS::registerSystem(std::unique_ptr<systems::ASystem>&& system)
+{
+    _systems.push_back(std::move(system));
+}
+
+void ECS::applySystems()
+{
+    for (const auto &system : _systems) {
+        system->apply(*this);
+    }
+}
