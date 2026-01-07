be1f6ddcf8636ce3069cb98c0c1aa971ba27e8d7
Lysandre B. <lysandre.boursette@epitech.eu>
2026-01-07 18:01:11 +0100

    tests(lib>rtnt): Drafted packet loss test.
    Packet loss simulation is done somewhere else, this test only tests if
    1000 packets can be sent in order.

---
+++ lib/rtnt/tests/tests/packet_loss.cpp
@@ -0,0 +1,70 @@
+#include <gtest/gtest.h>
+
+#include "network_fixture.hpp"
+
+namespace {
+
+struct Example
+{
+    static constexpr rtnt::core::packet::Id kId = 1801;
+    static constexpr rtnt::core::packet::Name kName = "EXAMPLE";
+    static constexpr rtnt::core::packet::Flag kFlag = rtnt::core::packet::Flag::kOrdered;
+
+    uint32_t x;
+
+    bool operator==(const Example&) const = default;
+
+    template <typename Archive>
+    void serialize(Archive& ar)
+    {
+        ar & x;
+    }
+};
+
+}  // namespace
+
+TEST_F(NetworkTest,
+       packet_loss)
+{
+    const uint32_t kPacketCount = 1000;
+
+    std::mutex dataMutex;
+    std::vector<uint32_t> receivedIndices;
+
+    server->getPacketDispatcher().bind<Example>([&](const auto& /*session*/, const Example& pkt) {
+        std::lock_guard lock(dataMutex);
+        LOG_INFO("Received packet, with body: [{}]", pkt.x);
+        receivedIndices.push_back(pkt.x);
+    });
+
+    client->connect("127.0.0.1", 4242);
+    ASSERT_TRUE(waitFor([&]() { return client->isConnected(); }, std::chrono::seconds(10)))
+        << "Client failed to connect.";
+
+    for (uint32_t i = 0; i < kPacketCount; ++i) {
+        Example pkt{.x = i};
+        LOG_INFO("Sending packet with body: [{}]", i);
+        client->send(pkt);
+    }
+
+    bool finished = waitFor(
+        [&]() {
+            std::lock_guard lock(dataMutex);
+            return receivedIndices.size() >= kPacketCount;
+        },
+        std::chrono::seconds(50));
+
+    ASSERT_TRUE(finished) << "Timeout: Only received " << receivedIndices.size() << "/"
+                          << kPacketCount << " packets.";
+
+    std::lock_guard lock(dataMutex);
+
+    ASSERT_EQ(receivedIndices.size(), kPacketCount);
+
+    LOG_INFO("Final list: {}", receivedIndices);
+
+    for (uint32_t i = 0; i < kPacketCount; ++i) {
+        ASSERT_EQ(receivedIndices[i], i)
+            << "Ordering failed! Index " << i << " was not the expected value.";
+    }
+}
