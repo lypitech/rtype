37e9f01381b43c0d80c9001be35e480c782af28f
Pierre Marguerie <pierre.marguerie@epitech.eu>
2026-01-08 18:12:20 +0100

    feat(graphical): Added `Texture` encapsulation

---
+++ Client/src/gui/texture.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include <raylib.h>
+
+#define PLAYER_TEXTURE_FILEPATH "../../Client/assets/sprites/Player.png"
+
+namespace gui {
+
+class Texture
+{
+public:
+    explicit Texture(const std::string& filepath,
+                     const float scale = 1)
+        : _texture(LoadTexture(filepath.c_str())),
+          _scale(scale)
+    {
+    }
+
+    ~Texture()
+    {
+        if (_texture.id != 0) {
+            UnloadTexture(_texture);
+        }
+    }
+
+    [[nodiscard]] const Texture2D& getTexture() const { return _texture; }
+    [[nodiscard]] const float& getScale() const { return _scale; }
+
+private:
+    Texture2D _texture;
+    double _scale;
+};
+
+}  // namespace gui
