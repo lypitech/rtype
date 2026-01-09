ab43df56bbd004cb31f5432b305b632b37123258
nL1x <nathan.jeannot@epitech.eu>
2026-01-09 01:38:28 +0100

    test(lib/rtecs): Added the first test of the ECS class.

--- lib/rtecs/tests/tests/ecs/ECS.cpp
+++ lib/rtecs/tests/tests/ecs/ECS.cpp
@@ -2,12 +2,24 @@
 
 #include <gtest/gtest.h>
 
+#include "fixtures/ECSFixture.hpp"
 #include "logger/Logger.h"
 #include "rtecs/sparse/group/SparseGroup.hpp"
 
+using namespace rtecs::tests::fixture;
 using namespace rtecs;
 
-// TEST(ECS, test)
-// {
-//
-// };
+TEST_F(ECSFixture, ecs_initialisation)
+{
+    _ecs.applySystems();
+
+    auto profileView = _ecs.group<Profile, Health>();
+
+    profileView.apply([](const Profile &profileComp, const Health &healthComp) {
+        if (healthComp.health < 10) {
+            EXPECT_STREQ(profileComp.prefix.data(), "[LOW]");
+        } else {
+            EXPECT_STREQ(profileComp.prefix.data(), "");
+        }
+    });
+};
