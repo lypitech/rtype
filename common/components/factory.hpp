86f7260a0d71e4a8cce6edd9592d6064dac9dca0
Babouye <esteban.bouyault-yvanez@epitech.eu>
2026-01-11 23:27:41 +0100

    feat(client>components): Added all new components to factory

--- common/components/factory.hpp
+++ common/components/factory.hpp
@@ -9,10 +9,27 @@
 #include "rteng.hpp"
 #include "rtnt/core/packet.hpp"
 #include "type.hpp"
+#include "health.hpp"
+#include "collision.hpp"
+#include "velocity.hpp"
+#include "damage.hpp"
+#include "invulnerability.hpp"
+#include "owner.hpp"
 
 namespace components {
 
-using GameComponents = rteng::ComponentsList<Position, Transform, Rectangle, Type>;
+using GameComponents = rteng::ComponentsList<
+    Position,
+    Transform,
+    Rectangle,
+    Type,
+    Health,
+    Collision,
+    Velocity,
+    Damage,
+    Invulnerability,
+    Owner
+>;
 
 class Factory
 {
