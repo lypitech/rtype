d14a48030d786c473ce81fec166dec2514f92fc9
Babouye <esteban.bouyault-yvanez@epitech.eu>
2026-01-11 23:26:52 +0100

    doc(client>components): Added comment in sprite component to keep in mind a possible scaling

--- Client/src/components/sprite.hpp
+++ Client/src/components/sprite.hpp
@@ -6,6 +6,11 @@ namespace components {
 struct Sprite
 {
     entity::Type type;
+
+    // Possible future extensions:
+    // float scale = 1.0f;
+    // std::string texture_path;   // Alternatively, we can use an ID to reference a texture in an asset manager
+                                // (btw that's better for performance and idk if it was intended with the enum above)
 };
 
 }  // namespace components
