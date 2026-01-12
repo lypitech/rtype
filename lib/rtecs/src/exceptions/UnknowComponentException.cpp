887af08b3aa48b5b1d5cc117147376fd9bc8dfcc
nL1x <nathan.jeannot@epitech.eu>
2026-01-12 14:01:09 +0100

    refactor(lib/rtecs): Removed exceptions

--- lib/rtecs/src/exceptions/UnknowComponentException.cpp
+++
@@ -1,8 +0,0 @@
-#include "rtecs/exceptions/UnknowComponentException.hpp"
-
-using namespace rtecs::exceptions;
-
-UnknowComponentException::UnknowComponentException(const types::ComponentID componentID)
-    : BaseException("The component with ID '" + std::to_string(componentID) + "' does not exist.")
-{
-}
