aeb0610ebe4501e1d501811ea1c3344973eefd48
nL1x <nathan.jeannot@epitech.eu>
2026-01-12 14:01:09 +0100

    refactor(lib/rtecs): Removed exceptions

--- lib/rtecs/src/exceptions/ComponentAlreadyExists.cpp
+++
@@ -1,8 +0,0 @@
-#include "rtecs/exceptions/ComponentAlreadyExists.hpp"
-
-using namespace rtecs::exceptions;
-
-ComponentAlreadyExists::ComponentAlreadyExists(const types::ComponentID componentID)
-    : BaseException("The component with ID '" + std::to_string(componentID) + "' already exists.")
-{
-}
