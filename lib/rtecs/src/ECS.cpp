e066b5c18239f35dc142b3ac7cb38f297f4abd20
nL1x <nathan.jeannot@epitech.eu>
2026-01-12 14:36:51 +0100

    feat(lib/rtecs/ecs): Added multiple methods - Added ECS::destroyEntity - Added ECS::getEntityMask - Added ECS::registerSystem (with a lambda or an ISystem) - Initialized the _componentMaskIndex to 0b1000000000000000000000000000000000000000000000000000000000000000

--- lib/rtecs/src/ECS.cpp
+++ lib/rtecs/src/ECS.cpp
@@ -1,12 +1,40 @@
 #include "rtecs/ECS.hpp"
 
-#include "rtecs/systems/ASystem.hpp"
+#include "rtecs/systems/ISystem.hpp"
+#include "rtecs/systems/SystemWrapper.hpp"
 
 using namespace rtecs;
 
-void ECS::registerSystem(std::unique_ptr<systems::ASystem>&& system) { _systems.push_back(std::move(system)); }
+ECS::ECS()
+    : _componentMaskIndex({std::bitset<64>{0b1000000000000000000000000000000000000000000000000000000000000000}})
+{
+}
 
-void ECS::applySystems()
+void ECS::registerSystem(std::unique_ptr<systems::ISystem>&& system) { _systems.push_back(std::move(system)); }
+
+void ECS::registerSystem(std::function<void(ECS& ecs)> applyFn, const std::string& name = "UnknowSystem")
+{
+    registerSystem(std::make_unique<systems::SystemWrapper>(applyFn, name));
+}
+
+const bitset::DynamicBitSet& ECS::getEntityMask(const types::EntityID entityId) const
+{
+    if (!_entities.contains(entityId)) {
+        return _emptyComponentMask;
+    }
+    return _entities.at(entityId);
+}
+
+void ECS::destroyEntity(types::EntityID entityId) const
+{
+    for (auto& _component : _components) {
+        if (_component.second->has(entityId)) {
+            _component.second->remove(entityId);
+        }
+    }
+}
+
+void ECS::applyAllSystems()
 {
     for (const auto& system : _systems) {
         system->apply(*this);
