aee097b8eff511b26b70156ff339848eebec59cc
Pierre Marguerie <pierre.marguerie@epitech.eu>
2026-01-13 15:31:50 +0100

    fix(server>level-director): Fixed only one wave spawning at a time when possible to spawn more

--- Server/src/level_director/level_director_utils.cpp
+++ Server/src/level_director/level_director_utils.cpp
@@ -4,15 +4,15 @@
 
 namespace level {
 
-const wave::Archetype* Director::pickRandomWave()
+std::vector<wave::Archetype*> Director::pickRandomWaves()
 {
-    std::vector<const wave::Archetype*> candidates;
+    std::vector<wave::Archetype*> candidates;
     size_t sum = 0;
     const size_t minSpend = _credits / 2;
 
     for (const auto& wave : _wavePool) {
         if (wave.difficultyCost <= _credits && wave.difficultyCost >= minSpend) {
-            candidates.push_back(&wave);
+            candidates.push_back(const_cast<std::vector<wave::Archetype*>::value_type>(&wave));
             sum += wave.weight;
         }
     }
@@ -21,27 +21,28 @@ const wave::Archetype* Director::pickRandomWave()
         sum = 0;
         for (const auto& wave : _wavePool) {
             if (wave.difficultyCost <= _credits) {
-                candidates.push_back(&wave);
+                candidates.push_back(const_cast<std::vector<wave::Archetype*>::value_type>(&wave));
                 sum += wave.weight;
             }
         }
     }
 
     if (candidates.empty() || sum == 0) {
-        return nullptr;
+        return {};
     }
 
     std::uniform_int_distribution<size_t> dist(1, sum);
+    std::vector<const wave::Archetype*> bought;
     size_t randomValue = dist(_rng);
 
     for (const auto* wave : candidates) {
         if (randomValue <= wave->weight) {
-            return wave;
+            bought.push_back(wave);
         }
         randomValue -= wave->weight;
     }
 
-    return candidates.back();
+    return {candidates.back()};
 }
 
 }  // namespace level
