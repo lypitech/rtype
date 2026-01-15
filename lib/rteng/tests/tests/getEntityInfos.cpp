f20e03be91b1eb8af724894efbe51e97aae0c54d
Pierre Marguerie <pierre.marguerie@epitech.eu>
2026-01-15 17:30:25 +0100

    feat(tests): Added a bunch of tests

--- lib/rteng/tests/tests/getEntityInfos.cpp
+++ lib/rteng/tests/tests/getEntityInfos.cpp
@@ -6,25 +6,77 @@ struct Position
 {
     int x;
     int y;
+
+    template <typename Archive>
+    void serialize(Archive& ar)
+    {
+        ar & x & y;
+    }
 };
 
 struct Type
 {
     int type;
+
+    template <typename Archive>
+    void serialize(Archive& ar)
+    {
+        ar & type;
+    }
 };
 
-TEST(getEntityInfos,
-     EngineUtilities)
+struct Name
+{
+    std::string name;
+
+    template <typename Archive>
+    void serialize(Archive& ar)
+    {
+        ar & name;
+    }
+};
+
+TEST(EngineUtilities,
+     getEntityInfosEntityWithAlltypes)
 {
     rteng::GameEngine engine = rteng::GameEngine(rteng::ComponentsList<Position, Type>{});
-    // std::vector<uint8_t> content{0, 0, 0, 0, 0, 0, 0, 0, 1};
-    //
-    // const rtecs::types::EntityID id = engine.registerEntity<Position, Type>(
-    //     nullptr,
-    //     {0, 0},
-    //     {1});
-    // const rteng::EntityInfos infos = engine.getEntityInfos(rteng::ComponentsList<Position, Type>{}, id);
-    //
-    // EXPECT_EQ(infos.first, engine.getEcs()->getEntityMask(id).serialize());
-    // EXPECT_EQ(infos.second, content);
+    std::vector<uint8_t> content{10, 0, 0, 0, 15, 0, 0, 0, 1, 0, 0, 0};
+    std::vector<uint64_t> mask{1, 2};
+
+    const rtecs::types::EntityID id = engine.registerEntity<Position, Type>(nullptr, {10, 15}, {1});
+    const rteng::EntityInfos infos =
+        engine.getEntityInfos(rteng::ComponentsList<Position, Type>{}, id);
+
+    EXPECT_EQ(infos.first, mask);
+    EXPECT_EQ(infos.second, content);
+}
+
+TEST(EngineUtilities,
+     getEntityInfosEntityWithoutSometypes)
+{
+    constexpr rteng::ComponentsList<Position, Type, Name> all{};
+    rteng::GameEngine engine(all);
+    std::vector<uint8_t> content{10, 0, 0, 0, 15, 0, 0, 0, 1, 0, 0, 0};
+    std::vector<uint64_t> mask{1, 2};
+
+    const rtecs::types::EntityID id = engine.registerEntity<Position, Type>(nullptr, {10, 15}, {1});
+    const rteng::EntityInfos infos = engine.getEntityInfos(all, id);
+
+    EXPECT_EQ(infos.first, mask);
+    EXPECT_EQ(infos.second, content);
+}
+
+TEST(EngineUtilities,
+     getEntityInfosEntityWithComplexType)
+{
+    constexpr rteng::ComponentsList<Position, Type, Name> all{};
+    rteng::GameEngine engine(all);
+    std::vector<uint8_t> content{1, 0, 0, 0, 5, 0, 'H', 'e', 'l', 'l', 'o'};
+    std::vector<uint64_t> mask{2, 3};
+
+    const rtecs::types::EntityID id = engine.registerEntity<Type, Name>(nullptr, {1}, {"Hello"});
+    const rteng::EntityInfos infos = engine.getEntityInfos(all, id);
+
+    EXPECT_EQ(infos.first, mask);
+    EXPECT_EQ(infos.second, content);
 }
