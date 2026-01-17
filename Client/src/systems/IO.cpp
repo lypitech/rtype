011887fd3dfbc1cc5c39c8a0255c64d31d1c9c70
Pierre Marguerie <pierre.marguerie@epitech.eu>
2026-01-17 12:28:22 +0100

    ref(client>systems>IO): Made the MouseButton bool --> button::State

--- Client/src/systems/IO.cpp
+++ Client/src/systems/IO.cpp
@@ -36,6 +36,23 @@ static button::State updateButtonState(const KeyboardKey key,
     return static_cast<button::State>(out);
 }
 
+static button::State updateMouseButtonState(const MouseButton key,
+                                            button::State previousState)
+{
+    constexpr auto DOWN_BIT = static_cast<uint8_t>(button::State::DOWN);
+
+    const bool isDown = IsMouseButtonDown(key);
+    const bool prevDown = (static_cast<uint8_t>(previousState) & DOWN_BIT) == DOWN_BIT;
+
+    uint8_t out = 0;
+    if (isDown != prevDown) {
+        out = button::STATE_CHANGED_BIT;
+    }
+    out |= static_cast<uint8_t>(isDown);
+
+    return static_cast<button::State>(out);
+}
+
 void IO::apply(rtecs::ECS&)
 {
     _input.up = updateButtonState(KEY_UP, _input.up);
@@ -43,11 +60,11 @@ void IO::apply(rtecs::ECS&)
     _input.left = updateButtonState(KEY_LEFT, _input.left);
     _input.right = updateButtonState(KEY_RIGHT, _input.right);
     _input.action1 = updateButtonState(KEY_SPACE, _input.action1);
-    _input.action2 = updateButtonState(KEY_LEFT_CONTROL, _input.action2);
+    _input.action2 = updateButtonState(KEY_R, _input.action2);
     _input.mouse.x = GetMouseX();
     _input.mouse.y = GetMouseY();
-    _input.mouse.leftButton = IsMouseButtonDown(MOUSE_LEFT_BUTTON);
-    _input.mouse.rightButton = IsMouseButtonDown(MOUSE_RIGHT_BUTTON);
+    _input.mouse.leftButton = updateMouseButtonState(MOUSE_LEFT_BUTTON, _input.mouse.leftButton);
+    _input.mouse.rightButton = updateMouseButtonState(MOUSE_RIGHT_BUTTON, _input.mouse.rightButton);
     packet::UserInput input{};
     if (_input.up == button::State::DOWN || _input.up == button::State::PRESSED) {
         input.input_mask |= static_cast<uint8_t>(game::Input::kUp);
@@ -60,6 +77,12 @@ void IO::apply(rtecs::ECS&)
     }
     if (_input.right == button::State::DOWN || _input.right == button::State::PRESSED) {
         input.input_mask |= static_cast<uint8_t>(game::Input::kRight);
+    }
+    if (_input.mouse.rightButton == button::State::PRESSED) {
+        input.input_mask |= static_cast<uint8_t>(game::Input::kShoot);
+    }
+    if (_input.action1 == button::State::PRESSED) {
+        _service.send(packet::Join{"A new player", 0});
     }
     if (input.input_mask == 0) {
         return;
