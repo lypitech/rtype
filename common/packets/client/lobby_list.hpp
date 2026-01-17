54e0364998d4f7ed884de4cee7d7baabdb339b0a
nL1x <nathan.jeannot@epitech.eu>
2026-01-17 17:00:55 +0100

    feat(common/packets): Added LobbyList and LobbyListAck packets

---
+++ common/packets/client/lobby_list.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include "enums/packets.hpp"
+#include "rtnt/core/packet.hpp"
+
+namespace packet {
+
+/**
+ * @struct packet::LobbyList
+ *
+ * @brief Ask the server for a list of lobby.
+ */
+struct LobbyList
+{
+    static constexpr auto kId = static_cast<rtnt::core::packet::Id>(type::Server::kDestroy);
+    static constexpr auto kFlag = rtnt::core::packet::Flag::kReliable;
+    static constexpr rtnt::core::packet::Name kName = "LOBBY_LIST";
+
+    uint32_t page;  ///< The current page
+
+    template <typename Archive>
+    void serialize(Archive& ar)
+    {
+        ar & page;
+    }
+};
+
+}  // namespace packet
+
---
+++ common/packets/server/lobby_list_ack.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include "enums/packets.hpp"
+#include "rtnt/core/packet.hpp"
+
+namespace packet {
+
+/**
+ * @struct packet::LobbyListAck
+ *
+ * @brief The response of the packet LobbyList.
+ */
+struct LobbyListAck
+{
+    static constexpr auto kId = static_cast<rtnt::core::packet::Id>(type::Server::kDestroy);
+    static constexpr auto kFlag = rtnt::core::packet::Flag::kReliable;
+    static constexpr rtnt::core::packet::Name kName = "LOBBY_LIST_ACK";
+
+    std::vector<uint32_t> roomIds;  ///< The list of rooms' id per page of 20 rooms
+    uint32_t page;                  ///< The current page
+    uint32_t maxPage;               ///< The max page
+
+    template <typename Archive>
+    void serialize(Archive& ar)
+    {
+        ar & roomIds & page & maxPage;
+    }
+};
+
+}  // namespace packet
