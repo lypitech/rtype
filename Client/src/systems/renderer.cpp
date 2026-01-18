504efbb84c07523c89ec4c6a1f8c28fcaaf18f60
Babouye <esteban.bouyault-yvanez@epitech.eu>
2026-01-18 12:44:27 +0100

    feat(client>systems): Added animation drawing in renderer

--- Client/src/systems/renderer.cpp
+++ Client/src/systems/renderer.cpp
@@ -1,5 +1,6 @@
 #include "renderer.hpp"
 
+#include "components/animations.hpp"
 #include "components/position.hpp"
 #include "components/sprite.hpp"
 #include "enums/entity_types.hpp"
@@ -7,9 +8,11 @@
 
 namespace systems {
 
-Renderer::Renderer(bool& shouldStop)
+Renderer::Renderer(bool& shouldStop,
+                   rteng::GameEngine& engine)
     : ASystem("Renderer"),
-      _closing(shouldStop)
+      _closing(shouldStop),
+      _engine(engine)
 {
     SetTraceLogLevel(LOG_NONE);
     InitWindow(1920, 1080, "Renderer");
@@ -29,11 +32,30 @@ void Renderer::apply(rtecs::ECS& ecs)
     BeginDrawing();
     ClearBackground({0, 255, 0, 255});
     ecs.group<components::Sprite, components::Position>().apply(
-        [this](const rtecs::types::EntityID&,
-               const components::Sprite& sprite,
-               const components::Position& pos) {
+        [&](const rtecs::types::EntityID& id,
+            const components::Sprite& sprite,
+            const components::Position& pos) {
             const gui::Texture& tex = _assetManager.getTexture(sprite.type);
-            DrawTextureEx(tex.getTexture(), {pos.x, pos.y}, 0.0f, tex.getScale(), WHITE);
+            Texture2D rawTex = tex.getTexture();
+
+            Rectangle sourceRec = {0.0f, 0.0f, (float)rawTex.width, (float)rawTex.height};
+
+            const auto animOpt =
+                ecs.group<components::Animation>().getEntity<components::Animation>(id);
+            if (animOpt.has_value()) {
+                const components::Animation& anim = animOpt.value().get();
+                sourceRec.x = anim.current_frame * anim.frame_width;
+                sourceRec.y = 0.0f;
+                sourceRec.width = (float)anim.frame_width;
+                sourceRec.height = (float)anim.frame_height;
+            }
+
+            Rectangle destRec = {pos.x,
+                                 pos.y,
+                                 sourceRec.width * (float)tex.getScale(),
+                                 sourceRec.height * (float)tex.getScale()};
+
+            DrawTexturePro(rawTex, sourceRec, destRec, {0, 0}, 0.0f, WHITE);
         });
     EndDrawing();
 }
