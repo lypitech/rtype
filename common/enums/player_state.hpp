45a2551bd11d6838338f2db875a9e1743cd066f3
nL1x <nathan.jeannot@epitech.eu>
2026-01-18 16:00:14 +0100

    refactor(server/enums/player_state.hpp): Renamed the namespace player::state to entity::state

--- common/enums/player_state.hpp
+++ common/enums/player_state.hpp
@@ -2,11 +2,11 @@
 
 #include <cstdint>
 
-namespace player::state {
+namespace entity::state {
 
 static constexpr uint8_t PlayerWaiting = 1;  ///< The player waiting for players to join
 static constexpr uint8_t PlayerReady = 2;    ///< The player is ready to start the game
-static constexpr uint8_t PlayerAlive = 2;    ///< The player is alive
-static constexpr uint8_t PlayerDead = 2;     ///< The player is dead
+static constexpr uint8_t EntityAlive = 3;    ///< The entity is alive
+static constexpr uint8_t EntityDead = 4;     ///< The entity is dead
 
-}  // namespace player::state
+}  // namespace entity::state
