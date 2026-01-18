7ba7af0a17d0fc4dca787d3ae1ebc87d411483b3
nL1x <nathan.jeannot@epitech.eu>
2026-01-18 15:59:23 +0100

    refactor(server/systems): Improved the way of moving entities

--- Server/src/systems/apply_movement.hpp
+++ Server/src/systems/apply_movement.hpp
@@ -4,16 +4,21 @@
 
 #include "components/hitbox.hpp"
 #include "components/position.hpp"
+#include "components/state.hpp"
+#include "components/type.hpp"
 #include "components/velocity.hpp"
 #include "rtecs/sparse/group/SparseGroup.hpp"
 #include "rtecs/systems/ASystem.hpp"
 #include "rtecs/types/types.hpp"
+
+using namespace components;
 
 namespace server::systems {
 
 class ApplyMovement final : public rtecs::systems::ASystem
 {
 private:
+    using Collider = std::tuple<Position&, Hitbox&, State&, Type&>;
     /**
      * @brief Check if a horizontal/vertical collision is detected.
      *
@@ -23,27 +28,39 @@ private:
      * @param otherBox The other hitbox
      * @return `true` if a collision is detected, `false` otherwise.
      */
-    static bool collide(const components::Position &refPos,
-                        const components::Hitbox &refBox,
-                        const components::Position &otherPos,
-                        const components::Hitbox &otherBox);
+    static bool collide(const Position& refPos,
+                        const Hitbox& refBox,
+                        const Position& otherPos,
+                        const Hitbox& otherBox);
 
-    static void handleHorizontalMovement(rtecs::types::EntityID id,
-                                         components::Position &pos,
-                                         const components::Velocity &vel,
-                                         const components::Hitbox &box,
-                                         rtecs::sparse::SparseGroup<components::Position,
-                                                                    components::Hitbox> &colliders);
-    static void handleVerticalMovement(rtecs::types::EntityID id,
-                                       components::Position &pos,
-                                       const components::Velocity &vel,
-                                       const components::Hitbox &box,
-                                       rtecs::sparse::SparseGroup<components::Position,
-                                                                  components::Hitbox> &colliders);
+    static std::optional<Collider> findCollider(rtecs::types::EntityID id,
+                                                const Position& pos,
+                                                const Hitbox& box,
+                                                rtecs::sparse::SparseGroup<Position,
+                                                                           Hitbox,
+                                                                           State,
+                                                                           Type>& colliders,
+                                                entity::Type expectedColliderType);
+
+    static void handleEntityMovement(Position& pos,
+                                     Velocity& vel,
+                                     const Hitbox& box,
+                                     State& state,
+                                     const std::optional<Collider>& collider);
+
+    static void handlePlayerHorizontalMovement(Position& pos,
+                                               Velocity& vel,
+                                               const Hitbox& box,
+                                               const std::optional<Collider>& collider);
+
+    static void handlePlayerVerticalMovement(Position& pos,
+                                             Velocity& vel,
+                                             const Hitbox& box,
+                                             const std::optional<Collider>& collider);
 
 public:
     explicit ApplyMovement();
-    void apply(rtecs::ECS &ecs) override;
+    void apply(rtecs::ECS& ecs) override;
 };
 
 }  // namespace server::systems
